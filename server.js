e0a6e330ec05ab8f6e596c089fd28956d595accf
sachin11336 <107074376+sachin11336@users.noreply.github.com>
2022-11-02 20:33:32 +0530

    Checkpoint 5

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const port = 9000;
 const http = require("http");
 const httpStatus = require("http-status-codes");
+const db = require("./models/index");
 
 const routeResponseMap = {
   "/movies": "All Movies Data in JSON format from Mongo DB",
@@ -26,3 +27,16 @@ const app = http.createServer((req, res) => {
 });
 
 app.listen(port);
+
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
